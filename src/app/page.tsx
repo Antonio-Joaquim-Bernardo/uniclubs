fe97812bc365884325c6e80ef21c02abb1cb85d8
Antonio-Joaquim-Bernardo <antoniojoaquimbernardo2005@gmail.com>
2026-05-01 09:22:42 +0100

    Feature: baixando e configurando os arquivos do Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1> OLÁ MUNDO! </h1>
+      <a href="c:\Users\AjB Enterprise\Pictures\ChatGPT Image 27 de jan. de 2026, 11_15_32.png">Link das Pu</a>
+    </div>
+  );
+}
